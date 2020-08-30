d81780b74d88be72430c5e9645e5234d4ce14cca
Adrian M.A <adrian@neurocadi.es>
2020-08-30 19:21:44 +0200

    Chat ready

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ import './src/config/mongoose.js';
 import cors from './src/middleware/cors.js';
 import usersRouter from './src/routes/users.js';
 import messagesRouter from './src/routes/messages.js';
+import http from 'http';
+import io from 'socket.io';
 
 const app = express();
+const httpApp =  http.Server(app);
+const socketIO = io(httpApp)
 const PORT = process.env.PORT || 3001;
 
 app.use(cors);
@@ -14,5 +18,24 @@ app.use(express.urlencoded({extended: true}));
 app.use('/users', usersRouter);
 app.use('/messages', messagesRouter)
 
+httpApp.listen(PORT, function() {
+    console.log('Server running on PORT ' + PORT);
+})
 
-app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+//Socket.io for chat
+socketIO.on('connection', function(socket) {
+
+    socket.on("user_join", function(data) {
+        socket.broadcast.emit("user_join", data);
+    });
+
+    socket.on("chat_message", function(data) {
+        socket.broadcast.emit("chat_message", data);
+    });
+
+    socket.on("disconnect", function(data) {
+        socket.broadcast.emit("user_leave", data);
+    });
+})
+
+// app.listen(PORT, () => console.log("Server running on PORT " + PORT));
